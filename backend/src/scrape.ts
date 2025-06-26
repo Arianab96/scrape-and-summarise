014d178903326fc915d035da63c42d91203632f1
Ariana Bakhshandeh <arianabakhshandeh@gmail.com>
2025-06-26 16:33:59 +0100

    add web scraping functionality with puppeteer

--- backend/src/scrape.ts
+++ backend/src/scrape.ts
@@ -1,8 +1,54 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import puppeteer from 'puppeteer';
+
 const router = express.Router()
 
-router.post('/scrape', (req, res) => {
-  res.send('Scrape complete')
-})
+router.post('/scrape', async (req: Request, res: Response) => {
+  try {
+  const userUrl = req.body.url
 
-export default router; 
+  if (!userUrl) {
+    return res.json({
+      success: false,
+      error: 'Please provide a valid URL'
+    });
+  }
+
+  // Open the browser
+  const browser = await puppeteer.launch();
+
+  // Open a new page
+  const page = await browser.newPage();
+
+  // Go to user's url
+  await page.goto(userUrl)
+
+  // Get the page title
+  const title = await page.title();
+
+  // Get the page content
+  const content = await page.evaluate(() => {
+    return document.body.innerText;
+  });
+
+ // Close the browser
+  await browser.close();
+
+  res.json({
+    success:true,
+    data: {
+      title: title,
+      content: content
+    }
+  });
+
+} catch (error) {
+    console.error('Scraping error:', error);
+    res.json({
+      success: false,
+      error: 'Error while scraping'
+    });
+  }
+});
+
+export default router;
